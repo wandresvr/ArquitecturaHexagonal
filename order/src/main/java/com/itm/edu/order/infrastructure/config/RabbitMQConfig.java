ea2caac81dad45544dfeb8a87b19597156b4de0b
Wilson Andrés Vargas Rojas <w.andres.vr@gmail.com>
2025-05-04 18:49:35 -0500

    config rabbitmq

--- order/src/main/java/com/itm/edu/order/infrastructure/config/RabbitMQConfig.java
+++ order/src/main/java/com/itm/edu/order/infrastructure/config/RabbitMQConfig.java
@@ -3,30 +3,28 @@ package com.itm.edu.order.infrastructure.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 
-
 @Configuration
 public class RabbitMQConfig {
-
     public static final String ORDER_QUEUE = "order.queue";
     public static final String ORDER_EXCHANGE = "order.exchange";
-    public static final String ORDER_ROUTING_KEY = "order.routingKey";
+    public static final String ORDER_ROUTING_KEY = "order.key";
 
     @Bean
     public Queue queue() {
-        return new Queue(ORDER_QUEUE);
+        return new Queue(ORDER_QUEUE, true);
     }
 
     @Bean
-    public TopicExchange exchange() {
-        return new TopicExchange(ORDER_EXCHANGE);
+    public DirectExchange exchange() {
+        return new DirectExchange(ORDER_EXCHANGE);
     }
 
     @Bean
-    public Binding binding(Queue queue, TopicExchange exchange) {
+    public Binding binding(Queue queue, DirectExchange exchange) {
         return BindingBuilder.bind(queue).to(exchange).with(ORDER_ROUTING_KEY);
     }
 }
--- stock/src/main/java/com/itm/edu/stock/infrastructure/messaging/RabbitOrderConsumer.java
+++ stock/src/main/java/com/itm/edu/stock/infrastructure/messaging/RabbitOrderConsumer.java
@@ -4,7 +4,6 @@ import com.itm.edu.common.dto.OrderMessageDTO;
 import com.itm.edu.stock.application.ports.input.ProcessOrderUseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -13,7 +12,7 @@ public class RabbitOrderConsumer {
 
     private final ProcessOrderUseCase processOrderUseCase;
 
-    @RabbitListener(queues = "${app.rabbitmq.queue}")
+    @RabbitListener(queues = "order.queue")
     public void onOrderMessage(OrderMessageDTO msg) {
         processOrderUseCase.processOrder(msg);
     }
